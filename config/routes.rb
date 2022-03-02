cc90b2168bd54e4c00f3fcfce026965c93d4874a
lrjbrual <lrjbrual@live.com>
2022-03-02 17:04:08 +0100

    adding login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   
-  root "projects#index"
-  resources :projects do 
-    resources :votes
-    resources :tasks
+  devise_scope :user do
+    root "projects#index"
+    resources :projects do 
+      resources :votes
+      resources :tasks
+    end
   end
+  # root 'devise/sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_151631) do
+ActiveRecord::Schema.define(version: 2022_02_25_160211) do
 
   create_table "projects", force: :cascade do |t|
     t.string "content"
@@ -26,6 +26,19 @@ ActiveRecord::Schema.define(version: 2022_01_28_151631) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "tasks", "projects"
